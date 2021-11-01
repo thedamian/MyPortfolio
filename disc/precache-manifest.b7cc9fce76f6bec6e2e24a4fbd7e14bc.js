52fbd95d26aaa6903aaed21b232634f4c66e71f0
damian <photoca@hotmail.com>
2021-11-01 16:32:15 -0400

    for github pages

---
+++ disc/precache-manifest.b7cc9fce76f6bec6e2e24a4fbd7e14bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c60b45b636b6a3002f71bb2f7aad8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df7b8c7e0f3ba8226ad4",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "154a76ffbcb5e1fc9c9e",
+    "url": "./static/js/2.3de4be71.chunk.js"
+  },
+  {
+    "revision": "2c26885e6fa22f75750640fb24eac16f",
+    "url": "./static/js/2.3de4be71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93dbee18eab1c49a331",
+    "url": "./static/js/3.ae32f5fe.chunk.js"
+  },
+  {
+    "revision": "df7b8c7e0f3ba8226ad4",
+    "url": "./static/js/main.73e9bc87.chunk.js"
+  },
+  {
+    "revision": "5c39759a88f5df3a88db",
+    "url": "./static/js/runtime-main.f45cd1b2.js"
+  }
+]);
